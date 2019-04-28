67621b8bb84f057ad0720f514e731666540bdaa7
m1c1b <viaznin123@gmail.com>
2019-04-28 23:50:41 +0500

    Remove duplicated directory

--- Test/Value.cs
+++
@@ -1,9 +0,0 @@
-namespace Test
-{
-    public class Value
-    {
-        public int Id { get; set; }      // Id значения
-        public double Val { get; set; }  // Значение
-        public string Time { get; set; } // Дата создания значения
-    }
-}
